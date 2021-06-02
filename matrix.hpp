88c615b92419f1754b290b2a17de455aff378d02
Charles <CPETER52@depaul.edu>
2021-06-02 12:21:06 -0500

    added matrix/vector multiplication/linear transformation method

--- matrix.hpp
+++ matrix.hpp
@@ -285,6 +285,32 @@ void matrixMultiplication(double (&matrixOne)[M][N], double (&matrixTwo)[N][P], 
 
 }
 
+/*
+ * 
+ * Implamentation of Matrix Vector Multiplication
+ *
+ *
+ *
+ * */
+template<int M, int N>
+void matrixVectorMultiplication(double (&matrix)[M][N], double ($vector)[N], double ($result)[M])
+{
+
+    for(int i = 0; i < M; i++){
+        double product = 0;
+        for(int j = 0; j < N; j++){
+           
+                product += matrix[i][j] * $vector[i];
+
+              
+            }
+                           
+        $result[i] = product;
+    }
+
+}
+
+
 
 
 /*
