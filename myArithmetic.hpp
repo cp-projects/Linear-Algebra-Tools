80529e3108a99da5df8b9e035982180192fe5ce0
Charles <CPETER52@depaul.edu>
2021-06-03 23:06:19 -0500

    fixing new header

--- myArithmetic.hpp
+++ myArithmetic.hpp
@@ -1,49 +1,17 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+#include <iostream>
 
 
 
-
-#if defined(_LP64)
-typedef int unknownNum_t;
-#elif
-typedef unsigned unknownNum_t;
-#elif
-typedef float unknownNum_t
-#elif
-typedef double unknownNum_t;
-#else
-typedef long unknownNum_t;
-#endif
-
-
-unknownNum_t test(unknownNum_t input)
+void test()
 {
 
+std::cout << "Hello World" << '\n';
 
 
-return input;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
