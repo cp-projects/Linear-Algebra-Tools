0b16aae2924f774b510ecb7859506fa2c8269b10
CPETER52 <cpeter52@depaul.edu>
2022-04-04 02:21:52 -0500

    renamed for matrix matrix addition and multiplication

--- new_organization/matrix.hpp
+++ new_organization/matrix.hpp
@@ -7,27 +7,6 @@
 #include "matrixErrors.hpp"
 
 
-
-/*
- *
- * Implamentation of a transpose method for a matrix of undefined size
- * 
- *
- * */
-template<unsigned R, unsigned C>
-void transpose(double (&original)[R][C], double (&transposed)[C][R])
-{
-
-
-     for(int i = 0; i < R; i++){
-         for(int j = 0; j < C; j++){
-             transposed[j][i] = original[i][j];}
-     }
-
-
-}
-
-
 /*
  * dot product
  *
