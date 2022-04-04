50f1c336513ac9ef5c967dad7706b3c95a1270e6
CPETER52 <cpeter52@depaul.edu>
2022-04-04 02:06:33 -0500

    added random matrix generation and changed name to reflect organization

--- new_organization/single_matrix_opps/cleanMatrix.cpp
+++ new_organization/single_matrix_opps/cleanMatrix.cpp
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 #include "cleanMatrix.hpp"
 #include "../errorsLA/matrixErrors.hpp"
 
@@ -24,6 +26,28 @@ void clearMatrix(double(&clear)[R][C]){
     for(int i = 0; i < R; i++)
         for(int j = 0; j < C; j++)
             clear[i][j] = 0;
+
+
+}
+
+
+template<unsigned R, unsigned C>
+void transpose(double (&original)[R][C], double (&transposed)[C][R])
+{
+
+     for(int i = 0; i < R; i++){
+         for(int j = 0; j < C; j++){
+             transposed[j][i] = original[i][j];}
+     }
+}
+
+
+template<unsigned R, unsigned C>
+void generateRandom(double(&random)[R][C]){
+
+    for(int i = 0; i < R; i++)
+        for(int j = 0; j < C; j++)
+	    random[i][j] = rand() % 70;
 
 
 }
--- new_organization/single_matrix_opps/cleanMatrix.hpp
+++ new_organization/single_matrix_opps/cleanMatrix.hpp
@@ -7,6 +7,12 @@ void generateIdentity(double(&identity)[R][C]);
 template<int R, int C>
 void clearMatrix(double(&clear)[R][C]);
 
+template<unsigned R, unsigned C>
+void transpose(double (&original)[R][C], double (&transposed)[C][R]);
+
+template<unsigned R, unsigned C>
+void generateRandom(double(&random)[R][C]);
+
 #include "cleanMatrix.cpp"
 
 #endif
