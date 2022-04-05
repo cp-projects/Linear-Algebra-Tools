e00bdffbd2b2ec80d0315d69b887c367f640dfe6
CPETER52 <cpeter52@depaul.edu>
2022-04-05 03:59:22 -0500

    created a centralized header as a first step toward combining the modules into a fully usable single library (ideally, .so)

---
+++ new_organization/libLA.hpp
@@ -0,0 +1,17 @@
+#ifndef __LaHeaders_H_
+#define __LaHeaders_H_
+
+#include "printLA/printLA.hpp"
+#include "errorsLA/matrixErrors.hpp"
+#include "myArithmetic/myArithmetic.hpp"
+#include "single_matrix_opps/single_matrix.hpp"
+
+#include "row_ops/row_ops.hpp"
+#include "matrix_matrix/matrix_matrix.hpp"
+#include "vector_vector/vector_vector.hpp"
+#include "matrix_vector/matrix_vector.hpp"
+
+#include "determinant/determinant.hpp"
+#include "elemination/gaussian.hpp"
+
+#endif
