5c6521f980bf0370537075cdb34d64852a7aaa49
cp-projects <cpeter52@depaul.edu>
2026-02-26 00:47:53 -0600

    Make row_ops and single_matrix_opps header only

--- include/rawArrays/libLA/row_ops/row_ops.hpp
+++ include/rawArrays/libLA/row_ops/row_ops.hpp
@@ -1,23 +1,95 @@
+#pragma once
+
 #ifndef __rowOps_H_
 #define __rowOps_H_
 
 
-template<int R, int C>
-void extractRow(int rowIndex, double (&original)[R][C], double rowVector[C]);
 
 template<int R, int C>
-void replaceRow(int rowIndex, double (&original)[R][C], double rowVector[C]);
+void extractRow(int rowIndex, double (&original)[R][C], double rowVector[C]){
+
+        for(int i = 0; i < R; i++){
+            for(int j = 0; j < C; j++){
+                if(i+1 == rowIndex) rowVector[j] = original[i][j];
+            }
+	}
+
+}
+
 
 template<int R, int C>
-void extractColumn(int columnIndex, double (&original)[R][C], double columnVector[R]);
+void replaceRow(int rowIndex, double (&original)[R][C], double rowVector[C]){
+
+        for(int i = 0; i < R; i++){
+            for(int j = 0; j < C; j++){
+                if(i+1 == rowIndex)  original[i][j] = rowVector[j];
+            }
+        }
+}
+
+
 
 template<int R, int C>
-void replaceColumn(int columnIndex, double (&original)[R][C], double columnVector[R]);
+void extractColumn(int columnIndex, double (&original)[R][C], double columnVector[R])
+{
+
+        for(int i = 0; i < R; i++){
+            for(int j = 0; j < C; j++){
+                if(j+1 == columnIndex) columnVector[i] = original[i][j];
+            }
+        }
+
+}
+
+
+
+
+template<int R, int C>
+void replaceColumn(int columnIndex, double (&original)[R][C], double columnVector[R])
+{
+
+        for(int i = 0; i < R; i++){
+            for(int j = 0; j < C; j++){
+                if(j+1 == columnIndex) original[i][j] = columnVector[i];
+            }
+        }
+
+}
+
+
 
 
 template<unsigned R, unsigned C>
-void swapRows(int rowOne, int rowTwo, double (&original)[R][C], double (&swapped)[R][C]);
+void swapRows(int rowOne, int rowTwo, double (&original)[R][C], double (&swapped)[R][C])
+{
+    double tempRow1[C];
+    double tempRow2[C];
+    for(int i = 0; i < R; i++){
+        for(int j = 0; j < C; j++){
+            if(i+1 == rowOne) tempRow1[j] = original[i][j];
+            if(i+1 == rowTwo) tempRow2[j] = original[i][j];
 
-#include "row_ops.cpp"
+            else{swapped[i][j] = original[i][j];}
+        }
+    }
+
+
+    for(int i = 0; i < R; i++){
+        for(int j = 0; j < C; j++){
+            if(i+1 == rowOne) swapped[i][j] = tempRow2[j];
+            if(i+1 == rowTwo) swapped[i][j] = tempRow1[j];
+        }
+    }
+}
+
+/*
+ *
+ * Remember to Make a swapCols
+ *
+ *
+ * */
+
+
+
 
 #endif
--- include/rawArrays/libLA/single_matrix_opps/single_matrix.hpp
+++ include/rawArrays/libLA/single_matrix_opps/single_matrix.hpp
@@ -1,18 +1,60 @@
+#pragma once
+
 #ifndef __singleMatrix_H_
 #define __singleMatrix_H_
 
-template<int R, int C>
-void generateIdentity(double(&identity)[R][C]);
+#include <stdlib.h>
+#include "../errorsLA/matrixErrors.hpp"
 
 template<int R, int C>
-void clearMatrix(double(&clear)[R][C]);
+void generateIdentity(double(&identity)[R][C]){
+
+    if(R != C) throw mustBeSquare();
+
+    for(int i = 0; i < R; i++){
+
+        for(int j = 0; j < C; j++){
+
+            if(i == j) identity[i][j] = 1;
+
+            else{ identity[i][j] = 0;}  }   }
+}
+
+
+
+
+template<int R, int C>
+void clearMatrix(double(&clear)[R][C]){
+
+    for(int i = 0; i < R; i++)
+        for(int j = 0; j < C; j++)
+            clear[i][j] = 0;
+
+
+}
+
 
 template<unsigned R, unsigned C>
-void transpose(double (&original)[R][C], double (&transposed)[C][R]);
+void transpose(double (&original)[R][C], double (&transposed)[C][R])
+{
+
+     for(int i = 0; i < R; i++){
+         for(int j = 0; j < C; j++){
+             transposed[j][i] = original[i][j];}
+     }
+}
+
 
 template<unsigned R, unsigned C>
-void generateRandom(double(&random)[R][C]);
+void generateRandom(double(&random)[R][C]){
 
-#include "single_matrix.cpp"
+    for(int i = 0; i < R; i++)
+        for(int j = 0; j < C; j++)
+	    random[i][j] = rand() % 70;
+
+
+}
+
+
 
 #endif
