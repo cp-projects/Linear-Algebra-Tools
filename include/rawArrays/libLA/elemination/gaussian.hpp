5991ba173b3b59655317406f90c19a38d785ea60
cp-projects <cpeter52@depaul.edu>
2026-04-25 14:00:52 -0500

    trying to fix the guassian elimination method and I am consistantly getting the garbage data problem, gonna try setting the matrix as 0 and then seeing from there

--- include/rawArrays/libLA/elemination/gaussian.hpp
+++ include/rawArrays/libLA/elemination/gaussian.hpp
@@ -1,9 +1,45 @@
+#pragma once
 #ifndef __guassian_H_
 #define __guassian_H_
 
-template<int R, int C>
-void gauss(double (&input)[R][C], double (&output)[R][C]);
+#include <iostream>
+#include "../myArithmetic/myArithmetic.hpp"
 
-#include "gaussian.cpp"
+template<int R, int C>
+void gauss(double (&input)[R][C], double (&output)[R][C])
+{
+
+int rowPivot = 0;
+int columnPivot = 0;
+
+double maxAbsolute;
+
+
+
+
+//works down main diagonal of the matrix
+while(rowPivot < R && columnPivot < C)
+    {
+
+
+	//should be absolute    
+	double abs =  input[rowPivot][columnPivot];
+
+	std::cout << abs << ' ';
+
+        if(abs > maxAbsolute)
+
+            maxAbsolute = input[rowPivot][columnPivot];
+            std::cout << " MAX VALUE = " << maxAbsolute << '\n';
+
+
+        rowPivot++;
+        columnPivot++;
+    }
+
+}
+
+
+
 
 #endif
--- include/rawArrays/libLA/elemination/test_elim.cpp
+++ include/rawArrays/libLA/elemination/test_elim.cpp
@@ -1,7 +1,17 @@
 #include "gaussian.hpp"
+#include "../single_matrix_opps/single_matrix.hpp"
+#include "../printLA/printLA.hpp"
 
 int main(){
 
+double test[4][4];
+generateRandom(test);
+std::cout << "I randomly generated a matrix called test, it is printed below: " << std::endl;
+printMatrix(test);
+
+double gaussOutput[4][4];
+gauss(test,gaussOutput);
+printMatrix(gaussOutput);
 
 return 0;
 }
